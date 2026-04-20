e22677668acfc1cf40d7a643a0424b57dacde0cb
Chris Johns <christopher.johns@howdeninsurance.co.uk>
2026-04-20 21:56:05 +0100

    Update home page to have background.

--- src/Mythfall.Client/Pages/HomePage.xaml.cs
+++ src/Mythfall.Client/Pages/HomePage.xaml.cs
@@ -6,5 +6,23 @@ public partial class HomePage : ContentPage
     {
         InitializeComponent();
     }
+
+    private async void OnCampaignClicked(object? sender, EventArgs e)
+    {
+        // TODO: Navigate to Campaign page
+        await DisplayAlertAsync("Campaign", "Campaign coming soon!", "OK");
+    }
+
+    private async void OnHeroesClicked(object? sender, EventArgs e)
+    {
+        // TODO: Navigate to Heroes page
+        await DisplayAlertAsync("Heroes", "Heroes roster coming soon!", "OK");
+    }
+
+    private async void OnShopClicked(object? sender, EventArgs e)
+    {
+        // TODO: Navigate to Shop page
+        await DisplayAlertAsync("Shop", "Shop coming soon!", "OK");
+    }
 }
 
